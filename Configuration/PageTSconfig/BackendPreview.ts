f881e4b4a1dbd2b17b6ed6d4dec49b31047b18a0
Nicole Cordes <typo3@cordes.co>
2018-07-15 21:06:36 +0200

    Add paths configuration for backend preview Fluid templates

---
+++ Configuration/PageTSconfig/BackendPreview.ts
@@ -0,0 +1,3 @@
+mod.web_layout.tt_content.preview.mymaskexport.templateRootPath = EXT:my_maskexport_sitepackage/Resources/Private/Backend/Templates/Content/
+mod.web_layout.tt_content.preview.mymaskexport.layoutRootPath = EXT:my_maskexport_sitepackage/Resources/Private/Backend/Layout/
+mod.web_layout.tt_content.preview.mymaskexport.partialRootPath = EXT:my_maskexport_sitepackage/Resources/Private/Backend/Partials/
