e3355a08d657fefb2bec9c1a626be6757afe75b7
Nicole Cordes <typo3@cordes.co>
2018-07-15 21:21:26 +0200

    Add locallang file to override EXT:my_mask_export/Resources/Private/Language/locallang_db.xlf

---
+++ ext_localconf.php
@@ -0,0 +1,5 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:my_mask_export/Resources/Private/Language/locallang_db.xlf']['my_maskexport_sitepackage'] =
+    'EXT:my_maskexport_sitepackage/Resources/Private/Language/mymaskexport_locallang_db.xlf';
