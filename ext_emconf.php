0965c504331b0fa87a9d88a7ba800fb7fa926f4b
Nicole Cordes <typo3@cordes.co>
2018-07-15 20:29:41 +0200

    Add basic extension files

---
+++ ext_emconf.php
@@ -0,0 +1,37 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "my_maskexport_sitepackage".
+ *
+ * Auto generated 15-07-2018 20:24
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'my_maskexport_sitepackage',
+  'description' => '',
+  'category' => 'fe',
+  'author' => '',
+  'author_email' => '',
+  'author_company' => '',
+  'state' => 'stable',
+  'version' => '0.1.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-8.7.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:2:{s:13:"composer.json";s:4:"9b09";s:12:"ext_icon.png";s:4:"03b4";}',
+);
+
