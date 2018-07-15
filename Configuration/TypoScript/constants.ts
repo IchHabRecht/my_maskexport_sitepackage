a8e7f1eaefd5a14b42b76d531472b39d574e617a
Nicole Cordes <typo3@cordes.co>
2018-07-15 20:43:33 +0200

    Add paths configuration for frontend Fluid templates

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -1,1 +1,5 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:my_mask_export/Configuration/TypoScript/constants.ts">
+
+plugin.tx_mymaskexport.view.templateRootPath = EXT:my_maskexport_sitepackage/Resources/Private/Templates/Content/
+plugin.tx_mymaskexport.view.partialRootPath = EXT:my_maskexport_sitepackage/Resources/Private/Partials/
+plugin.tx_mymaskexport.view.layoutRootPath = EXT:my_maskexport_sitepackage/Resources/Private/Layouts/
