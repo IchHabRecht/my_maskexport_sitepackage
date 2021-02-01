180234681e249f6737a3861bc59aea66b1329f7e
Nicole Cordes <typo3@cordes.co>
2021-02-01 20:11:42 +0100

    Move content elements to own NewContentElementWizard tab

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -6,3 +6,9 @@ defined('TYPO3_MODE') || die('Access denied.');
     'Configuration/PageTSconfig/BackendPreview.tsconfig',
     'my_maskexport_sitepackage: Backend preview templates'
 );
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'my_maskexport_sitepackage',
+    'Configuration/PageTSconfig/NewContentElementWizard.tsconfig',
+    'my_maskexport_sitepackage: New content element wizard'
+);
