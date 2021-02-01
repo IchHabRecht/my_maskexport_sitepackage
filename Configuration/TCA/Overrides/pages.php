600f4728c2389d9a993328a1e224ec439c1d1f69
Nicole Cordes <typo3@cordes.co>
2021-02-01 19:58:28 +0100

    Update compatibility to TYPO3 9.5 - 10.4

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -3,6 +3,6 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
     'my_maskexport_sitepackage',
-    'Configuration/PageTSconfig/BackendPreview.ts',
+    'Configuration/PageTSconfig/BackendPreview.tsconfig',
     'my_maskexport_sitepackage: Backend preview templates'
 );
--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '7.6.0-8.7.99',
+      'typo3' => '9.5.0-10.4.99',
       'my_mask_export' => '0.1.0',
     ),
     'conflicts' => 
